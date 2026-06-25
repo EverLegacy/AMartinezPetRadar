18f02514cb12276566df954e420f2e209b1dbf3a
Chimichungus <147010173+EverLegacy@users.noreply.github.com>
2026-06-24 23:29:52 -0600

    Remove CacheInterceptor from found pets controller
    Removed CacheInterceptor from FoundPetsController.

--- src/found-pets/found-pets.controller.ts
+++ src/found-pets/found-pets.controller.ts
@@ -1,10 +1,8 @@
-import { Body, Controller, Post, Get, UseInterceptors, } from "@nestjs/common";
-import { CacheInterceptor } from "@nestjs/cache-manager";
+import { Body, Controller, Post, Get, } from "@nestjs/common";
 import { CreateFoundPetDto } from "./dto/create-found-pet.dto";
 import { FoundPetsService } from "./found-pets.service";
 
 @Controller("found-pets")
-@UseInterceptors(CacheInterceptor)
 export class FoundPetsController {
     constructor(private readonly foundPetsService: FoundPetsService) {}
 
